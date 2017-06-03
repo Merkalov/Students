c86ae253c6323e5c94cac882e460780ff066dde4
Merkalov <makar79rus@gmail.ru>
2017-06-03 23:21:07 +1000

    fix path css file.

--- views/list.php
+++ views/list.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <title>Список студентов</title>
-    <link href="http://students/public/css/style_list.css" rel="stylesheet" type="text/css">
+    <link href="../public/css/style_list.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 <!--<header>Список студентов</header>-->
