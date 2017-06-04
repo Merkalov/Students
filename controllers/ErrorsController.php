f8fdf770c94f8a15e1933e50ed0b1da401bd6495
Merkalov <makar79rus@gmail.ru>
2017-06-04 13:03:07 +1000

    fix

--- controllers/ErrorsController.php
+++ controllers/ErrorsController.php
@@ -1,37 +1,35 @@
 <?php
 
 namespace Controllers;
-
+use Models\Errors;
 class ErrorsController
 {
     public static function actionShowErrors()
     {
         ob_start();
-        $err = [];
-        $err = $_SESSION['err'];
-        require_once(ROOT . '\views\error_show.php');
+        $parameters = [];
+        $parameters['err'] = $_SESSION['err'];
+        echo FrontController::_render('error_show', $parameters);
         ob_end_flush();
     }
 
     public function actionNoAccess()
     {
         ob_start();
-        require_once(ROOT . '\views\no_access.php');
-        if (isset($_POST['login'])) {
-            header('Location: /login');
-        }
-        if (isset($_POST['register'])) {
-            header('location: /register');
-        }
+        echo FrontController::_render('no_access', []);
+        if (isset($_POST['login']))
+            FrontController::_redirect('login', 301);
+        if (isset($_POST['register']))
+            FrontController::_redirect('register', 301);
         ob_end_flush();
     }
 
     public function actionNotFound()
     {
         ob_start();
-        require_once(ROOT . '\views\404.php');
+        echo FrontController::_render('404', []);
         if (isset($_POST['ok'])) {
-            header('Location: /login');
+            FrontController::_redirect('login', 301);
         }
         ob_end_flush();
     }
